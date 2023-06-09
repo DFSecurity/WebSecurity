8718e489b4d24be00f78031b41852a324d41ea79
Demétrio de Freitas Oliveira <81397961+DFSecurity@users.noreply.github.com>
2023-06-09 01:36:22 -0300

    Update send2.php

--- send2.php
+++ send2.php
@@ -2,24 +2,43 @@
 
     include ('connect.php');
 
-    $name = addslashes ($_POST ['name']);
-    $email = $_POST ['email'];
-    $password = md5 (addslashes ($_POST ['password']));
-    $result = "insert into cadastrar (name, email, password) values ('$name', '$email', '$password');";
-    $create = "create table cadastrar (id int primary key auto_increment, name varchar (100) not null, email varchar (100) not null, password varchar (32) not null);";
+    if ($_SERVER ["REQUEST_METHOD"] == "POST") {
 
-    if ($connect -> query ($create)) {
-        if (mysqli_query ($connect, $result)) {
-            echo "Usuário cadastrado com sucesso!";
+        $name = addslashes ($_POST ['name']);
+        $email = addslashes ($_POST ['email']);
+        $password = md5 (addslashes ($_POST ['password']));
+        $confirm = md5 (addslashes ($_POST ['confirm']));
+    
+        if ($password === $confirm && $confirm == $password) {
+
+            $result = "insert into cadastrar (name, email, password, confirm) values ('$name', '$email', '$password', '$confirm');";
+            $create = "create table cadastrar (id int primary key auto_increment, name varchar (100) not null, email varchar (100) not null, password varchar (32) not null, confirm varchar (32) not null);";
+
+            if ($connect -> query ($create)) {
+                
+                if (mysqli_query ($connect, $result)) {
+                    echo "Usuário cadastrado com sucesso!";
+                } else {
+                    echo "Error";
+                }
+
+            } else if (mysqli_query ($connect, $result)) {
+                
+                echo "Usuário cadastrado com sucesso!";
+            
+            } else {
+
+                echo "Error";
+            }
+
+            mysqli_close ($connect);
+
         } else {
-            echo "Error";
+            
+            echo "A senha e confirmar senha não coincidem!";
+        
         }
-    } else if (mysqli_query ($connect, $result)) {
-        echo "Usuário cadastrado com sucesso!";
-    } else {
-        echo "Error";
+    
     }
- 
-    mysqli_close ($connect);
 
 ?>
