1eb52a17fd1971b5ce8ae2b28f5af1787b73048e
Demétrio de Freitas Oliveira <81397961+DFSecurity@users.noreply.github.com>
2023-06-17 16:52:09 -0300

    Update cadastrar.php

--- cadastrar.php
+++ cadastrar.php
@@ -10,16 +10,16 @@
 	<h2> Tela de cadastrar </h2>
 	<br>
 	<form method="POST" action="send2.php">
-	    <input type="text" name="name" placeholder="Usuario">
+	    <input type="text" name="name" required placeholder="Usuário">
             <br> <br>
-	    <input type="email" name="email" placeholder="E-mail">
+	    <input type="email" name="email" required placeholder="E-mail">
 	        <br> <br>
-        <input type="password" name="password" placeholder="Senha">
-            <br> <br>
-        <input type="password" name="confirm" placeholder="Confirmar senha">
-            <br> <br>
-        <a href="login.php"> <input value="Voltar" class="voltar" name="voltar"> </a>
-        <button type="submit" value="cadastrar" class="cadastrar" name="cadastrar"> Cadastrar </button>
+            <input type="password" name="password" required placeholder="Senha">
+                <br> <br>
+            <input type="password" name="confirm" required placeholder="Confirmar senha">
+                <br> <br>
+            <a href="login.php"> <input value="Voltar" class="voltar" name="voltar"> </a>
+            <button type="submit" value="cadastrar" class="cadastrar" name="cadastrar"> Cadastrar </button>
         </form>
     </div>
 </body>
